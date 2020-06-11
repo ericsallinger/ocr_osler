d7a45375ccaf700002f9ee32ef4caa2d551af33e
ericsallinger <eric.sallinger303@gmail.com>
2020-06-11 17:37:35 -0600

    working on being able to upload images

--- ocr_osler/imagebase/models.py
+++ ocr_osler/imagebase/models.py
@@ -6,3 +6,4 @@ from model_utils.models import TimeStampedModel
 class Imagebase(TimeStampedModel):
         name = models.CharField("test string", max_length=255)
         slug = AutoSlugField("test slug", unique=True, always_update=False, populate_from="name")
+        thumbnail = models.ImageField(default='default.png',blank=True)
