0d19aa80998d1958ad9c297bc0dea00da95bf449
Charles Lu <lucharles@wustl.edu>
2020-06-24 17:42:42 -0400

    Update async.py

--- ocr_osler/users/management/commands/async.py
+++ ocr_osler/users/management/commands/async.py
@@ -1,5 +1,9 @@
 from django.core.management.base import BaseCommand
 
+# Handles two tasks, recieve_image and return_message. To avoid task queue of
+# OCR taking too long and resulting in a timeout, we
+# return a message asynchrously
+
 
 class Command(BaseCommand):
     help = 'Runs task asynchrously'
